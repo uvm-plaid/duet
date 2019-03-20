eb4c336d11b8346544875f306a3db64f7c10e928
Chike Abuah <abuahchu@gmail.com>
2019-03-20 12:41:25 -0400

    mean+stddev

--- experiments/accuracy.rb
+++ experiments/accuracy.rb
@@ -1,12 +1,33 @@
 
+module Enumerable
+  def sum
+    self.inject(0){|accum, i| accum + i }
+  end
+
+  def mean
+    self.sum/self.length.to_f
+  end
+
+  def variance
+    m = self.mean
+    sum = self.inject(0){|accum, i| accum +(i-m)**2 }
+    sum/(self.length - 1).to_f
+  end
+
+  def std_dev
+    Math.sqrt(self.variance)
+  end
+end
+
 # cat in.csv | cut -d, -f1
 # sed 's/\([^,]*\),\(.*\)/\2/'  Input_file
 
 puts "running"
+RUNS = 5
 SAMPLE = 10 # ← limited testing, 50
 LR = 1
-ITERS = 1000
-ϵs = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100][0..1] # ← limited testing
+ITERS = 100 # ← limited testing, 1000
+ϵs = [0.001, 0.01, 0.1, 1, 10][0..1] # ← limited testing
 run = "examples/gd-pb-mini.ed.duet"
 xs = "data_short/ffxs.csv"
 ys = "data_short/ffys.csv"
@@ -19,8 +40,20 @@ def accuracy(ϵ,run,xs,ys)
   al = accl.split(",").map(&:to_f)
   acc = al[0]/(al[0]+al[1])
 end
-as = ϵs.map { |ϵ| accuracy(ϵ,run,xs,ys) }
 
-puts as
+runs = []
+ams = []
+asds = []
+trials = ϵs.length
+RUNS.times do |i|
+  runs << ϵs.map { |ϵ| accuracy(ϵ,run,xs,ys) }
+end
+trials.times do |i|
+  ams << runs.map { |l| l[i] }.mean
+  asds << runs.map { |l| l[i] }.std_dev
+end
+
+puts "MEANs: #{ams}"
+puts "STDDEVs: #{asds}"
 
 puts "done"
